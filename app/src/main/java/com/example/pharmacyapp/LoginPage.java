55cdc66b886733e087c30a46701ab68fc8f3169a
YasiruOps <yasirunayanasaraopanayaka@gmail.com>
2021-11-20 19:08:18 +0530

    reg page validations

--- app/src/main/java/com/example/pharmacyapp/LoginPage.java
+++ app/src/main/java/com/example/pharmacyapp/LoginPage.java
@@ -2,13 +2,113 @@ package com.example.pharmacyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginPage extends AppCompatActivity {
+
+    TextView signuplink;
+    EditText varmail, varpass;
+    Button varlogin;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        varmail = findViewById(R.id.enter_mail);
+        varpass = findViewById(R.id.enter_pass);
+        varlogin = findViewById(R.id.loginbtn);
+        fAuth = FirebaseAuth.getInstance();
+
+        //Login
+
+        varlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //check if user already logged in
+                if(fAuth.getCurrentUser() != null){
+                    startActivity(new Intent(getApplicationContext(), HomePage.class));
+                    finish();
+                }
+
+                String email = varmail.getText().toString().trim();
+                String password = varpass.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    varmail.setError("Email required");
+                    Toast.makeText(LoginPage.this, "Please enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    varpass.setError("Password reqiured");
+                    Toast.makeText(LoginPage.this, "Please enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginPage.this, "Logged in Sucessfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), HomePage.class));
+                        } else{
+                            Toast.makeText(LoginPage.this, "Error" +task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+        //Colors part of text
+        SpannableString ss = new SpannableString("New User? Create Account");
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View textView) {
+                startActivity(new Intent(LoginPage.this, RegisterPage.class));
+            }
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(false);
+            }
+        };
+        ss.setSpan(clickableSpan, 10, 24, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        TextView textView = (TextView) findViewById(R.id.sign_up_pg_link);
+        textView.setText(ss);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setHighlightColor(Color.TRANSPARENT);
+
+        //SIGN UP PAGE LINK
+//        signuplink = findViewById(R.id.sign_up_pg_link);
+//        signuplink.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(getApplicationContext(), RegisterPage.class));
+//            }
+//        });
     }
 }
--- app/src/main/java/com/example/pharmacyapp/RegisterPage.java
+++ app/src/main/java/com/example/pharmacyapp/RegisterPage.java
@@ -2,13 +2,120 @@ package com.example.pharmacyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterPage extends AppCompatActivity {
+
+    EditText varname, varemail, varcontact, varpass, varconfpass;
+    Button varreg_btn;
+    TextView loginpglink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        varname = findViewById(R.id.reg_name);
+        varemail =findViewById(R.id.reg_email);
+        varcontact = findViewById(R.id.reg_contact);
+        varpass = findViewById(R.id.reg_pass);
+        varconfpass = findViewById(R.id.reg_conf_pass);
+
+
+        varreg_btn = findViewById(R.id.register_Btn);
+        varreg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name, email, contact, password, confpass;
+                boolean field_chck = true;
+
+                name = varname.getText().toString().trim();
+                email = varemail.getText().toString().trim();
+                contact = varcontact.getText().toString().trim();
+                password = varpass.getText().toString().trim();
+                confpass = varconfpass.getText().toString().trim();
+
+                if(TextUtils.isEmpty(name)  || TextUtils.isEmpty(password)) {
+                    varname.setError("Please fill in you all details");
+                    field_chck = false;
+                    return;
+                }
+                if (TextUtils.isEmpty(email)){
+                    varemail.setError("Please enter email");
+                    field_chck = false;
+                    return;
+                }
+                if (TextUtils.isEmpty(contact)){
+                    varcontact.setError("Please enter contact");
+                    field_chck = false;
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    varcontact.setError("Please enter contact");
+                    field_chck = false;
+                    return;
+                }
+                if (TextUtils.isEmpty(confpass)){
+                    varconfpass.setError("Please enter password");
+                    field_chck = false;
+                    return;
+                }
+
+                if (!password.equals(confpass)){
+                    Toast toast = Toast.makeText(RegisterPage.this, "password mismatch", Toast.LENGTH_SHORT);
+                    TextView view = (TextView) toast.getView().findViewById(android.R.id.message);
+                    view.setTextColor(Color.RED);
+                    toast.show();
+                    field_chck = false;
+                    return;
+                }
+
+                if (field_chck == true) {
+
+                }
+            }
+        });
+
+        //Colors part of text
+        SpannableString ss = new SpannableString("Already have an account? Login");
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View textView) {
+                startActivity(new Intent(RegisterPage.this, LoginPage.class));
+            }
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(false);
+            }
+        };
+        ss.setSpan(clickableSpan, 26, 30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        TextView textView = (TextView) findViewById(R.id.login_page_link);
+        textView.setText(ss);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setHighlightColor(Color.TRANSPARENT);
+
+        loginpglink = findViewById(R.id.login_page_link);
+        loginpglink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginPage.class));
+            }
+        });
     }
 }
